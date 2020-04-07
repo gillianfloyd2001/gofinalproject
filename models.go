9d0f14bf6f2861db354fa332089a70905c580685
Gillian Floyd <gillianfloyd2003@icloud.com>
2020-04-07 15:52:54 -0500

    added json markers and finished the structs

--- models.go
+++ models.go
@@ -6,34 +6,37 @@ import (
 
 // This creates an Employee
 type Employee struct {
-	ID       uint   `json:"id"`
+	Id       uint   `json:"id"`
 	Name     string `json:"name"`
 	Position string `json:"position"`
 }
 
 // This creates a ClockIn
 type Clockin struct {
-	ID            uint      `json: "id"`
-	EmployeeID    *Employee `json:"employeeId"`
+	Id            uint      `json:"id"`
+	EmployeeId    *Employee `json:"employeeId"`
 	TimeClockedIn time.Time `json:"timeClockedIn"`
 }
 
 // This creates a clockout
 type Clockout struct {
-	ID             uint      `json: "id"`
-	EmployeeId     *Employee `json: "employeeId"`
-	Timeclockedout time.Time `json: "timeClockedOut"`
+	Id             uint      `json:"id"`
+	EmployeeId     *Employee `json:"employeeId"`
+	Timeclockedout time.Time `json:"timeClockedOut"`
 }
 
 // This creates a Delivery
 type Delivery struct {
-	ID uint `json: "id"`
+	Id          uint   `json:"id"`
+	PhoneNumber string `json: "phoneNumber"`
+	Address     string `json:"address"`
+	Tip         bool   `json:"tip"`
 }
 
 // This creates a Payout
 type Payout struct {
-	ID         uint `json: "id"`
-	EmployeeID *Employee
+	Id         uint `json: "id"`
+	EmployeeId *Employee
 	Timestamp  time.Time
 	Amount     uint
 }
